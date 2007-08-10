1ceb8cab29f0ad03ca2d58542709293f78cbb840
Wolfgang Oertl <wiz@luaforge.net>
2007-08-10 20:01:56 +0000

    Present forecast as table.  Use °C instead of C.  Show date only if not equal to today.

--- weather/weather.lua
+++ weather/weather.lua
@@ -1,14 +1,14 @@
 #! /usr/bin/lua
--- vim:sw=4:sts=4
+-- vim:sw=4:sts=4:encoding=utf8
 --
 -- Demonstration program to download and display weather information from weather.com.
 -- by Wolfgang Oertl
 --
 -- Revisions:
 --  2007-08-06	first version: show a few facts about the current weather.
+--  2007-08-10	forecast as table
 --
 -- TODO:
---  - display forecast nicer: as a table
 --  - configure locations
 --  - configure metric/imperial units
 --  - configure what fields to show/display nicely
@@ -23,6 +23,7 @@ require "gtk.strict"
 
 local forecast_days = 5
 
+-- handler for the mainwin.destroy signal
 function on_mainwin_destroy()
     gtk.main_quit()
 end
@@ -36,19 +37,18 @@ function on_location_changed(box)
     local iter = gtk.new "GtkTreeIter"
     box:get_active_iter(iter)
     local code = store:get_value(iter, 1)
-    get_weather_info(code)
-end
 
-function get_weather_info(code)
     gtk.http_co.request_co{
 	host = "xoap.weather.com",
-	uri = "/weather/local/" .. code .. "?cc=*&unit=m&dayf=" .. forecast_days,
+	uri = "/weather/local/" .. code .. "?cc=*&unit=m&dayf="..forecast_days,
 	callback = weather_info_callback,
     }
 end
 
+--
+-- Some progress on receiving.  When done, process the data.
+--
 function weather_info_callback(arg, ev, data1, data2, data3)
-    -- print("CALLBACK", arg, ev, data1, data2, data3)
     if ev == 'done' then
 	parse_weather_info(arg.sink_data)
     end
@@ -66,6 +66,7 @@ function parse_weather_info(data)
 
     table.insert(stack, {items={}})
 
+    -- split into lines...
     for line in data:gmatch("([^\n]+)") do
 	line_nr = line_nr + 1
 	-- print("LINE", line_nr, line)
@@ -79,6 +80,11 @@ function parse_weather_info(data)
 
     -- build a second representation, using the tag names as keys
     xml = transform_xml(stack[1].items)
+
+    if xml.weather.head.ut == "C" then
+	xml.weather.head.ut = "°C"
+    end
+
     -- dump_it(xml)
 
     present_weather(xml)
@@ -133,31 +139,82 @@ function dump_it(stack, prefix)
     end
 end
 
+local function _new_lbl(tbl, top, left, right, s)
+    local lbl = gtk.label_new(s)
+    lbl:set_use_markup(true)
+    tbl:attach_defaults(lbl, left, right, top, top + 1)
+    return lbl
+end
+
+
 --
--- The weather response has been parsed into a nice tree.  Extract the interesting
--- fields and fill the GUI.
+-- The weather response has been parsed into a nice tree.  Extract the
+-- interesting fields and fill the GUI.
 --
 function present_weather(xml)
     local s
     local cw = mainwin.currweather
     local buf = cw:get_buffer()
+    local dt = parse_date(xml.weather.cc.lsup)
+    local mydt = os.date("%Y-%m-%d")
 
-    s = xml.weather.loc.dnam .. " at " .. xml.weather.cc.lsup .. "\n"
+    -- build the current weather information as a string    
+    s = xml.weather.loc.dnam .. " at " .. dt.h .. ":" .. dt.mn .. " "
+    if dt.dt ~= mydt then s = s .. dt.dt .. " " end
+    s = s .. dt.rest .. "\n"
     s = s .. "Temp: " .. xml.weather.cc.tmp .. " " .. xml.weather.head.ut.."\n"
     s = s .. "Description: " .. xml.weather.cc.t .. "\n"
     s = s .. "Humidity: " .. xml.weather.cc.hmid .. "%\n"
 
     buf:set_text(s, string.len(s))
 
+    -- remove previous forecast table, if it exists.
+    local tmp = mainwin.forecast:get_child()
+    if tmp then tmp:destroy() end
+
+    -- if a forecast has been retrieved, show it.
     if xml.weather.dayf then
-	s = ""
+	cnt = #xml.weather.dayf.day
+	tbl = gtk.table_new(3, cnt * 3, false)
 	for i, day in ipairs(xml.weather.dayf.day) do
-	    s = s .. day.t .. " " .. day.dt .. ": "
-		.. day.low .. " to " .. day.hi .. " "
-		.. xml.weather.head.ut .. "\n"
+	    local col = i * 3
+
+	    -- day label
+	    _new_lbl(tbl, 0, col, col + 2, "<span weight=\"bold\">" .. day.t
+		.. "</span>")
+
+	    -- precipitation
+	    _new_lbl(tbl, 1, col, col + 1, day.part[1].ppcp .. "%")
+	    _new_lbl(tbl, 1, col+ 1, col + 2, day.part[2].ppcp .. "%")
+
+	    -- temperature
+	    _new_lbl(tbl, 2, col, col + 1, "<span foreground=\"#ff0000\">"
+		.. day.hi .. "</span> " .. xml.weather.head.ut)
+	    _new_lbl(tbl, 2, col + 1, col + 2, "<span foreground=\"#0000ff\">"
+		.. day.low .. "</span> " .. xml.weather.head.ut)
+
+	    -- separator
+	    if i < cnt then
+		tbl:attach_defaults(gtk.vseparator_new(), col + 2, col + 3,
+		    0, 3)
+	    end
 	end
-	mainwin.forecast:get_buffer():set_text(s, #s)
+	mainwin.forecast:add(tbl)
+	tbl:show_all()
     end
+end
+
+--
+-- weather.com returns date/time in this format: mm/dd/yy hh:mm [AM|PM]
+-- parse that and return it in a better format.
+--
+function parse_date(s)
+    local m, d, y, h, mn, ampm, rest = s:match(
+	"^(%d+)/(%d+)/(%d+) (%d+):(%d+) ([AP]M) (.*)")
+    if ampm == "PM" then h = h + 12 end
+    if y+0 < 2000 then y = y + 2000 end
+    return { dt=string.format("%04d-%02d-%02d", y, m, d), h=h, mn=mn,
+	rest=rest }
 end
 
 function main()
