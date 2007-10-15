1379ab6572019a7c89d5588a6a04311d108387a9
Wolfgang Oertl <wiz@luaforge.net>
2007-10-15 14:36:41 +0000

    - functionality of hash-fch-generate2 merged in - all data structures but one are now static - at runtime the cmph library's hash function and the data file with the   serialized hash data is used, instead of the compiled in version; makes   building and the makefile simpler - support all hash algorithms of cmph, not just jenkins - bugfix: store the full hash value, not just the bucket number of the first   hash function

--- lua-gtk/src/hash-fch-generate.c
+++ lua-gtk/src/hash-fch-generate.c
@@ -1,23 +1,50 @@
 /** vim:sw=4:sts=4
- * Given a data file as produced by CMPH using the FCH algorithm, create
- * a compileable C file from it.  This is just the hash function, even
- * though it includes a lookup table.
+ *
+ * Convert a text file with (key,value) pairs and the corresponding CMPH file
+ * into a compileable C file.  When compiled, it exports one symbol,
+ * which is hash_info_{prefix}.
+ *
+ * This only works for the FCH algorithm of cmph.
+ *
+ * Given the generated (and compiled) FCH function, read the list of keys
+ * the associated value, and write the hash table.  Each bucket contains
+ * exactly one entry:
+ *
+ *	bytes	    contents
+ *	4	    hash value of the name
+ *	2	    offset of the data in the data string
+ *
+ * The data string contains the actual data.  Note that the FCH hash is not
+ * order preserving; this means that each key maps to a distinct bucket number,
+ * but in an undefined order.
+ *
+ * Following steps are required:
+ *  - read all key/data pairs
+ *  - calculate the bucket number for each key to determine the order
+ *  - write the buckets (data) in this order sequentially into a string
+ *  - write an index table with one offset per bucket
+ *
+ * Copyright (C) 2007 Wolfgang Oertl
  */
 
-#include <cmph.h>
-#include <cmph_structs.h>
-#include <fch_structs.h>
-#include <jenkins_hash.h>
+#include "cmph_structs.h"   // cmph_t definition
+#include "fch_structs.h"    // jenkins_state_t, __fch_data_t
 
-#include <string.h>
-#include <errno.h>
+#define INLINE
+#include "hash-fch.h"	    // hash_$(HASHFCH)
+
+#include <string.h>	    // strlen, strchr, strdup, memset
+#include <errno.h>	    // errno
+
+
+// line buffer length.
+static int buf_len = 200;
 
 /**
  * Output the data structure.
  * Required fields:
  *  h1, h2, m, b, p1, p2, g
  */
-
 void fch_dump(cmph_t *mphf, const char *prefix)
 {
     if (mphf->algo != CMPH_FCH) {
@@ -40,7 +67,7 @@ void fch_dump(cmph_t *mphf, const char *prefix)
     printf("/* max. value in g is %d */\n", maxval);
 
     printf("#include \"hash-fch.h\"\n");
-    printf("const struct my_fch %s = {\n", prefix);
+    printf("static const struct my_fch _%s_fch = {\n", prefix);
     printf("  m: %d,\n", f->m);
     printf("  b: %d,\n", f->b);
     printf("  g_size: %d,\n", g_size);
@@ -73,17 +100,230 @@ void fch_dump(cmph_t *mphf, const char *prefix)
 }
 
 
+
+/**
+ * Calculate the string length, but \[0-7]{1,3} is considered as just one
+ * character; this is how the C compiler sees it later.
+ *
+ * Note: \n, \t etc. is also detected, although no check for invalid escape
+ * sequences is done.
+ */
+int special_strlen(const char *s)
+{
+    int len = 0, i;
+
+    while (*s) {
+	if (*s++ == '\\') {
+	    if (*s >= 'a' && *s <= 'z')
+		s ++;
+	    else {
+		// a \000 octal sequence is max. 3 digits.
+		for (i=3; i && *s >= '0' && *s <= '7'; i--)
+		    s ++;
+	    }
+	}
+	len ++;
+    }
+
+    return len;
+}
+
+
+/**
+ * Output one hash entry.  This consists of the hash value followed by a two
+ * or four bytes offset to the data.  The information is printed as individual
+ * octal bytes.
+ */
+void print_hash_entry(unsigned int hash_value, unsigned int data_offset,
+    int offset_size)
+{
+    int j;
+    static int cnt = 0;
+
+    cnt += 4;
+    for (j=4; j; j--) {
+	printf("\\%o", hash_value & 0xff);
+	hash_value >>= 8;
+    }
+
+    cnt += offset_size;
+    for (j=offset_size; j; j--) {
+	printf("\\%o", data_offset & 0xff);
+	data_offset >>= 8;
+    }
+
+    if (cnt > 20) {
+	printf("\"\n \"");
+	cnt = 0;
+    }
+}
+
+/**
+ * Given the already generated FCH hash function, read the list of keys and
+ * the associated value, and write the hash table.  Each bucket contains
+ * exactly one entry:
+ *
+ *	bytes	    contents
+ *	4	    hash value of the name
+ *	2	    offset of the data in the data string
+ *
+ * The data string contains the actual data.
+ */
+int build_hash_table(cmph_t *mphf, const char *fname, const char *prefix)
+{
+    int bucket_nr, len, keylen, line=0, i;
+    FILE *f;
+    char *buf, *key, *data, **data_table;
+    unsigned int data_offset=0, offset_size, hash_value;
+    unsigned int *hash_table, *entry;
+
+    f = fopen(fname, "r");
+    if (!f) {
+	fprintf(stderr, "Can't open %s: %s\n", fname, strerror(errno));
+	return 2;
+    }
+
+    buf = (char*) malloc(buf_len);
+
+    int keys = cmph_size(mphf);
+
+    // for each bucket, the hash value.
+    hash_table = (unsigned int*) malloc(keys * sizeof(*hash_table));
+    memset(hash_table, 0, keys * sizeof(*hash_table));
+
+    // for each bucket, the data
+    data_table = (char**) malloc(keys * sizeof(*data_table));
+    memset(data_table, 0, keys * sizeof(*data_table));
+
+    for(;;) {
+	key = fgets(buf, buf_len, f);
+	if (!key)
+	    break;
+	line ++;
+
+	// line should contain at least a newline!
+	len = strlen(key);
+	if (len == 0) {
+	    fprintf(stderr, "Nothing read on line %d\n", line);
+	    break;
+	}
+
+	// should end with "\n", else line was truncated.
+	if (key[len-1] != '\n') {
+	    fprintf(stderr, "Line truncated at line %d.  Please increase "
+		"the buffer size (currently %d)\n", line, buf_len);
+	    return 1;
+	}
+
+	// chop off the newline
+	len --;
+	key[len] = 0;
+
+	// split into key and data part
+	data = strchr(key, ',');
+	if (!data) {
+	    fprintf(stderr, "No data part on line %d\n", line);
+	    continue;
+	}
+	*data = 0;
+	data ++;
+	keylen = data - key - 1;
+	len -= keylen + 1;
+
+	// calculate the bucket number
+	bucket_nr = cmph_search(mphf, key, keylen);
+	if (bucket_nr < 0 || bucket_nr >= keys) {
+	    fprintf(stderr, "Error: %d buckets, key %.*s maps to bucket #%d\n",
+		keys, keylen, key, bucket_nr);
+	    return 1;
+	}
+
+	// which must be empty.
+	entry = hash_table + bucket_nr;
+	if (*entry) {
+	    fprintf(stderr, "Collision at %d\n", bucket_nr);
+	    break;
+	}
+
+	// Calculate the first hash value - again, it already happened in
+	// cmph_search, but it doesn't return it anywhere.  This is an
+	// unfortunate intrusion into cmph internals!
+	struct __fch_data_t *fch = (struct __fch_data_t*) mphf->data;
+	hash_value = hash(fch->h1, key, keylen);
+	
+	// store hash value and the data.
+	*entry = hash_value;
+	data_table[bucket_nr] = strdup(data);
+    }
+
+    free(buf);
+
+    // output the data table, thereby compute the total length of the data.
+    data_offset = 0;
+    printf("static const unsigned char _%s_data[] =\n", prefix);
+    for (i=0; i<keys; i++) {
+	printf("  \"%s\"\n", data_table[i]);
+	data_offset += special_strlen(data_table[i]);
+    }
+    printf(";\n\n");
+
+    /* output the hash table */
+    offset_size = (data_offset < 65536) ? 2 : 4;
+    printf("static const unsigned char _%s_hash[] = \n \"", prefix);
+	
+    data_offset = 0;
+    for (i=0; i<keys; i++) {
+	entry = hash_table + i;
+	print_hash_entry(*entry, data_offset, offset_size);
+	data_offset += special_strlen(data_table[i]);
+    }
+
+    // Sentry so that the data size calculation will work for the last bucket.
+    print_hash_entry(0, data_offset, offset_size);
+    printf("\";\n\n");
+
+    // Output the master structure.
+    printf(
+	"const struct hash_info hash_info_%s = {\n"
+	"  hash_func: &_%s_fch,\n"
+	"  data_table: _%s_data,\n"
+	"  hash_table: _%s_hash,\n"
+	"  offset_size: %d,\n"
+	"};\n", prefix, prefix, prefix, prefix, offset_size);
+
+    return 0;
+}
+
+
+/**
+ * Check that only supported algorithms are used.
+ *
+ * @return 0 on success, 1 on error.
+ */
+int validate_mphf(cmph_t *mphf)
+{
+    if (mphf->algo != CMPH_FCH) {
+	fprintf(stderr, "algorithm is not FCH, but %s\n",
+	    cmph_names[mphf->algo]);
+	return 1;
+    }
+
+    return 0;
+}
+
+
 /**
  * Load the given file using the cmph_load function of the cmph library,
- * then call the dump function.
+ * then write the hash function data; then read the (key,value) file and
+ * write that out, too, in the correct order.
  */
 int main(int argc, char **argv)
 {
     FILE *f;
     cmph_t *mphf;
 
-    if (argc != 3) {
-	fprintf(stderr, "Usage: %s {input file} {prefix}\n", argv[0]);
+    if (argc != 4) {
+	fprintf(stderr, "Usage: %s {cmph file} {keyfile} {prefix}\n", argv[0]);
 	return 1;
     }
 
@@ -97,9 +337,19 @@ int main(int argc, char **argv)
     mphf = cmph_load(f);
     fclose(f);
 
-    /* dump the fch data */
-    fch_dump(mphf, argv[2]);
+    if (!mphf) {
+	fprintf(stderr, "Input file %s contains no valid cmph data.\n",
+	    argv[1]);
+	return 3;
+    }
 
-    return 0;
+    if (validate_mphf(mphf))
+	return 4;
+
+    /* dump the fch data */
+    fch_dump(mphf, argv[3]);
+
+    // read the data, map in the correct order, write it
+    return build_hash_table(mphf, argv[2], argv[3]);
 }
 
