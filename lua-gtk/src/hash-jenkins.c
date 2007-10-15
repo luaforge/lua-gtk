3071f91d40b458351043b03ce104767947dd8e93
Wolfgang Oertl <wiz@luaforge.net>
2007-10-15 17:57:34 +0000

    Fix for amd64: use int instead of long int for 32 bit values.

--- lua-gtk/src/hash-jenkins.c
+++ lua-gtk/src/hash-jenkins.c
@@ -8,8 +8,8 @@
  */
 
 
-typedef  unsigned long  int  ub4;   /* unsigned 4-byte quantities */
-typedef  unsigned       char ub1;   /* unsigned 1-byte quantities */
+typedef  unsigned int  ub4;   /* unsigned 4-byte quantities */
+typedef  unsigned char ub1;   /* unsigned 1-byte quantities */
 
 #define hashsize(n) ((ub4)1<<(n))
 #define hashmask(n) (hashsize(n)-1)
