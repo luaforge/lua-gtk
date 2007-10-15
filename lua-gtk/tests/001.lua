13577b374191554160374b2308b9017c916c9d7f
Wolfgang Oertl <wiz@luaforge.net>
2007-10-15 14:41:44 +0000

    window_new requires the proper ENUM value, not an integer.

--- lua-gtk/tests/001.lua
+++ lua-gtk/tests/001.lua
@@ -4,7 +4,7 @@ gtk.init(nil, nil)
 
 -- test that arbitrary values can be stored in a widget.
 
-win = gtk.window_new(0)
+win = gtk.window_new(gtk.GTK_WINDOW_TOPLEVEL)
 win._foo = 1
 assert(win._foo == 1)
 
