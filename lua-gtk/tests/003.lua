2fd10b1362a7aa4fe9a265d3f070de4ae32be67f
Wolfgang Oertl <wiz@luaforge.net>
2007-10-19 10:45:36 +0000

    Test "-" operator for flags, i.e. unset bits.

--- lua-gtk/tests/003.lua
+++ lua-gtk/tests/003.lua
@@ -13,7 +13,15 @@ assert(tostring(v) == "GtkWidgetFlags:can-default")
 v = v + gtk.GTK_REALIZED
 assert(tostring(v) == "GtkWidgetFlags:realized|can-default")
 
+-- assignment
+w = v
+assert(tostring(w) == "GtkWidgetFlags:realized|can-default")
+
 -- can't add flags of different types
 v = v + gtk.GTK_STATE_NORMAL
 assert(v == nil)
+
+-- unset flags
+w = w - gtk.GTK_CAN_DEFAULT
+assert(tostring(w) == "GtkWidgetFlags:realized")
 
