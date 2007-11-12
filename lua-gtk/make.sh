0fb4c643f24145c79137789d4b041e453c48c915
Wolfgang Oertl <wiz@luaforge.net>
2007-11-12 10:53:39 +0000

    Remember the build platform (linux, win32 etc.)

--- lua-gtk/make.sh
+++ lua-gtk/make.sh
@@ -1,6 +1,21 @@
 #! /bin/sh
+#
+# Run make for the selected target.  You can select one by specifying it
+# as first parameter; the current state is stored in a state file.
+#
 
 TARGET="linux"
+T=""
+STATE="make.state"
+
+# read current target
+if test -f $STATE; then
+	T=$(<$STATE)
+	if test "$T"; then
+		TARGET=$T
+	fi
+fi
+
 case "$1" in
 	linux|archlinux|win32) TARGET="$1"; shift ;;
 esac
@@ -8,6 +23,11 @@ esac
 if test ! -f script/Makefile.$TARGET; then
 	echo "Unknown target $TARGET"
 	exit 1
+fi
+
+# store new state if changed
+if test "$T" != "$TARGET"; then
+	echo "$TARGET" > $STATE
 fi
 
 exec make -f script/Makefile.$TARGET "$@"
