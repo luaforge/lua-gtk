042a4ae512bf5b1af42ea2aebaaa9df9ab58ffb7
Wolfgang Oertl <wiz@luaforge.net>
2007-11-21 16:48:07 +0000

    Test refcounting related to aliases and GtkComboBox/GtkListStore.

---
+++ lua-gtk/tests/006.lua
@@ -0,0 +1,14 @@
+#! /usr/bin/lua
+-- vim:sw=4:sts=4
+
+require "gtk"
+
+gtk.init(0)
+w = gtk.combo_box_new()
+ls = gtk.list_store_new(2, gtk.G_TYPE_INT, gtk.G_TYPE_STRING)
+w:set_model(ls)
+
+-- Creates an alias, because this function returns GtkTreeModel for the
+-- existing GtkListStore widget.
+ls2 = w:get_model()
+
