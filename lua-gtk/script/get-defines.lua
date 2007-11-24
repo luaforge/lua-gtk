40f21a8a049b6479b04b2ef75d52f3b92f159c1c
Wolfgang Oertl <wiz@luaforge.net>
2007-11-24 23:51:14 +0000

    - parse glib-2.0/gobject/gtype.h - properly support string and integer constants

--- lua-gtk/script/get-defines.lua
+++ lua-gtk/script/get-defines.lua
@@ -6,47 +6,55 @@
 -- ENUMs.
 -- Copyright (C) 2007 Wolfgang Oertl
 
+require "bit"
+
+-- add the directory where this Lua file is in to the package search path.
+package.path = package.path .. ";" .. string.gsub(arg[0], "%/[^/]+$", "/?.lua")
+require "common"
+
 enums = {}
 
 function parse_file(fname)
     local line2 = ""
 
     for line in io.lines(fname) do
-	if string.match(line, "\\$") then
-	    line2 = line2 .. string.sub(line, 1, -2)
-	else
+	-- emulate "continue" command
+	while true do
+	    -- continuation
+	    if string.match(line, "\\$") then
+		line2 = line2 .. string.sub(line, 1, -2)
+		break
+	    end
+
 	    line = line2 .. line
 	    line2 = ""
 
+	    -- string defines
 	    local name, value = string.match(line,
 		"^#define ([A-Z_]+) +\"([^\"]+)\"")
 	    if name and value then
-		enums[name] = encode_value(value)
+		print(encode_enum(name, value, 0))
+		break
 	    end
+
+	    -- G_TYPE defines
+	    name, value = string.match(line,
+		"^#define ([A-Z0-9_]+)%s+G_TYPE_MAKE_FUNDAMENTAL%s+%((%d+)%)")
+	    if name and value then
+		-- *4 is what G_TYPE_MAKE_FUNDAMENTAL does.
+		print(encode_enum(name, value * 4, 0))
+		break
+	    end
+
+	    -- nothing usable in this line, skip.
+	    break
 	end
     end
 end
 
-function encode_value(val)
-    local s = ""
-
-    if type(val) == "string" then
-	s = '\\000\\000' .. val
-    else
-	error("unhandled type " .. type(val) .. " in encode_value")
-    end
-
-    return s
-end
-
-function write_output(ofile)
-    for k, v in pairs(enums) do
-	print(string.format("%s,%s", k, v))
-    end
-end
-
 -- main --
-path = "/usr/include/gtk-2.0"
-parse_file(path .. "/gtk/gtkstock.h")
-write_output()
+path_gtk = "/usr/include/gtk-2.0"
+path_glib = "/usr/include/glib-2.0"
+parse_file(path_gtk .. "/gtk/gtkstock.h")
+parse_file(path_glib .. "/gobject/gtype.h")
 
