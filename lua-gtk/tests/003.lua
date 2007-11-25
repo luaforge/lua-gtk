17303fb1a71ff0837cabd97c4fae6dff32aff8c3
Wolfgang Oertl <wiz@luaforge.net>
2007-11-25 00:06:15 +0000

    - update for recent changes - test integer and string constants

--- lua-gtk/tests/003.lua
+++ lua-gtk/tests/003.lua
@@ -1,11 +1,11 @@
 #! /usr/bin/lua
 require "gtk"
-gtk.init(nil, nil)
+gtk.init()
 
 -- test ENUMs
 
 v = gtk.GTK_CAN_DEFAULT
-assert(tostring(v) == "GtkWidgetFlags:can-default")
+assert(tostring(v) == "GtkWidgetFlags:can-default", "v is " .. tostring(v))
 
 -- the addition is really a bitwise or
 v = v + gtk.GTK_CAN_DEFAULT
@@ -17,11 +17,19 @@ assert(tostring(v) == "GtkWidgetFlags:realized|can-default")
 w = v
 assert(tostring(w) == "GtkWidgetFlags:realized|can-default")
 
--- can't add flags of different types
-v = v + gtk.GTK_STATE_NORMAL
-assert(v == nil)
+-- can't add flags of different types - must raise an error
+rc, msg = pcall(function() v = v + gtk.GTK_STATE_NORMAL end)
+assert(rc == false)
 
 -- unset flags
 w = w - gtk.GTK_CAN_DEFAULT
 assert(tostring(w) == "GtkWidgetFlags:realized")
+
+-- integer constants
+v = gtk.G_TYPE_INT
+assert(type(v) == "number")
+
+-- string constants
+v = gtk.GTK_STOCK_OPEN
+assert(v == "gtk-open")
 
