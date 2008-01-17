bfb61517e4659139426ec6128bd2f90d5ac8c028
Wolfgang Oertl <wiz@luaforge.net>
2008-01-17 18:14:53 +0000

    - code moved to tests/003.lua

--- lua-gtk/tests/010.lua
+++
@@ -1,7 +0,0 @@
-
-require "gtk"
-
--- ENUMs can now be converted to numbers.  Can be useful when trying to
--- compare the result of gtk_dialog_run() with these constants.
-assert(gtk.GTK_RESPONSE_OK:tonumber() == -5)
-
