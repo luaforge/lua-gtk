975b8b0b76e2e9df6109daafebb494fb4979415d
Wolfgang Oertl <wiz@luaforge.net>
2008-01-17 18:14:01 +0000

    - use a TextView instead of a Label. - experimental UTF8 conversion, seems to work

--- lua-gtk/tests/005.lua
+++ lua-gtk/tests/005.lua
@@ -10,30 +10,51 @@ require "gtk.socket_co"
 require "gtk.watches"
 
 lbl = nil
+buf = nil
 
 function http_download()
     local val, msg, sock, gio, condition
+    local host = "www.google.at"
 
     -- This call blocks, unfortunately, during the DNS lookup.  No GUI updates
     -- during this time.
-    lbl:set_text("Connecting...")
-    gio, msg = gtk.socket_co.connect("www.google.com", 80, false)
+    local s = "Connecting..."
+    buf:set_text(s, #s)
+    gio, msg = gtk.socket_co.connect(host, 80, false)
     if not gio then return gio, msg end
 
-    lbl:set_text("Connected!");
+    s = "Connected!"
+    buf:set_text(s, #s)
     sock = msg
 
-    gtk.socket_co.write_chars(gio, "GET / HTTP/1.1\nConnection:close\n\n")
+    gtk.socket_co.write_chars(gio, "GET / HTTP/1.1\nHost: " .. host
+	.. "\nConnection: close\n\n")
+
+    -- set up conversion to utf8
+    conv = gtk.g_iconv_open("UTF8", "latin1")
 
     -- read the response in many small pieces, with small delays in between.
+    s = ""
     while true do
 	val, msg = gtk.socket_co.read_chars(gio, 50)
 	if not val then break end
-	-- print("got data with length", #val)
-	lbl:set_text(lbl:get_text() .. val)
-	-- coroutine.yield("sleep", 100)
-    end
 
+	-- print("got data with length", #val)
+	obuf = string.rep(' ', #val)
+	a, b, c, d, e = gtk.g_iconv(conv, val, #val, obuf, #obuf)
+	print(a, b, c, d, e)
+
+
+	-- local val2, x, y, err = gtk.g_locale_from_utf8(val, #val, nil, nil, nil)
+	--if val2 then
+--	    s = s .. val2
+--	    buf:set_text(s, #s)
+--	else
+--	    print("UTF8 conversion error at ", string.sub(val, x+1, x+10))
+--	    print("Info", err.message)
+--	end
+	-- coroutine.yield("sleep", 100)
+    end 
     print("end", msg)
 end
 
@@ -42,34 +63,42 @@ function background_task()
     local cnt = 15
 
     for i = 1, cnt do
-	lbl:set_text(string.format("Sleeping... %d/%d", i, cnt))
+	local s = string.format("Sleeping... %d/%d", i, cnt)
+	buf:set_text(s, #s)
 	coroutine.yield("sleep", 100)
     end
     http_download()
-    coroutine.yield("sleep", 500)
+    coroutine.yield("sleep", 5000)
     gtk.main_quit()
 end
 
 -- create a minimal GUI: a window with a label in it.
 function init_gui()
     local w = gtk.window_new(gtk.GTK_WINDOW_TOPLEVEL)
-    w:connect('destroy', function() gtk.main_quit() end)
-    lbl = gtk.label_new("")
-    w:add(lbl)
+    w:set_title("HTTP GET Test")
+    w:connect('destroy', gtk.gtk_main_quit)
+    w:set_default_size(300, 300)
+    local sw = gtk.scrolled_window_new(nil, nil)
+    w:add(sw)
+    local lbl = gtk.text_view_new()
+    buf = lbl:get_buffer()
+    sw:add(lbl)
     w:show_all()
 end
 
 -- return true if to continue calling this idle function; false otherwise.
-function idle(a, b, c)
-    print("idle", a[1], b, c)
-    a[1] = a[1] + 1
-    return a[1] < 10
+function idle(tbl)
+    print("idle", unpack(tbl))
+    tbl[1] = tbl[1] + 1
+    return tbl[1] < 10
 end
 
-gtk.init()
 init_gui()
 gtk.watches.start_watch(background_task)
 
-gtk.g_idle_add(idle, {1}, 2, 3)
+-- This function expects a "void*" argument, which will be passed to the
+-- callback.  lua-gtk allows nil, widgets and any other data type to be
+-- used.
+gtk.g_idle_add(idle, {1, 2, 3})
 gtk.main()
 
