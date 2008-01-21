9b3d823b90298b2f9c8b1af5e1c038fabc67dd75
Wolfgang Oertl <wiz@luaforge.net>
2008-01-21 07:00:46 +0000

    - use a proper Makefile instead of the make.sh, making building easier.

--- lua-gtk/make.sh
+++ lua-gtk/make.sh
@@ -1,33 +1,5 @@
 #! /bin/sh
-#
-# Run make for the selected target.  You can select one by specifying it
-# as first parameter; the current state is stored in a state file.
-#
+# obsolete; just type make
 
-TARGET="linux"
-T=""
-STATE="make.state"
-
-# go to my own directory.
-cd ${0%/*}
-
-# read current target
-if test -f $STATE; then
-	T=$(<$STATE)
-	if test "$T"; then
-		TARGET=$T
-	fi
-fi
-
-if test "$1" -a "$1" != "common" -a -f "script/Makefile.$1"; then
-	TARGET=$1
-	shift
-fi
-
-# store new state if changed
-if test "$T" != "$TARGET"; then
-	echo "$TARGET" > $STATE
-fi
-
-exec make -f "script/Makefile.$TARGET" "$@"
+make "$@"
 
