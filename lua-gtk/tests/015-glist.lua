0a72611ca7ea312c9708758dc7ac2db7e8c3e848
Wolfgang Oertl <wiz@luaforge.net>
2008-02-04 15:30:41 +0000

    - test various functions on GList, e.g. append, delete, sort, reverse

--- lua-gtk/tests/015-glist.lua
+++ lua-gtk/tests/015-glist.lua
@@ -1,6 +1,16 @@
 #! /usr/bin/env lua
 require "gtk"
 
+-- Helper to show the elements of a list.  It uses the foreach function, which
+-- requires callbacks from C to Lua, which requires closures; this is actually
+-- tricky, but works :)
+function list2string(ls)
+   local t = {}
+   ls:foreach(function(x) t[#t + 1] = x end, nil)
+   return table.concat(t, ' ')
+end
+
+
 -- create a new g_list with one element
 ls = gtk.g_list_append(nil, 10)
 assert(ls:length() == 1)
@@ -10,11 +20,27 @@ assert(ls:length() == 1)
 ls:append(20)
 ls:append(30)
 
--- test the foreach function, which requires callbacks from C to Lua,
--- which requires closures; this is actually tricky, but works :)
-sum = 0
-ls:foreach(function(x) sum = sum + x end, nil)
-assert(sum == 60)
+assert(list2string(ls) == "10 20 30")
 
--- ok
+x = ls:nth(1)
+assert(x.data == 20)
+
+-- test reversal
+ls = ls:reverse()
+assert(list2string(ls) == "30 20 10")
+
+
+-- test removal
+ls:delete_link(x)
+assert(list2string(ls) == "30 10")
+
+-- add some more
+ls:append(15)
+ls:append(3)
+ls:append(94)
+
+-- sort using a callback (in a closure)
+ls = ls:sort(function(a, b) return a - b end)
+assert(list2string(ls) == "3 10 15 30 94")
+
 
