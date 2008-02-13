84e0e8be88079bda5d60de7756840b875d51d27d
Wolfgang Oertl <wiz@luaforge.net>
2008-02-13 21:40:50 +0000

    - comments added with hints what to do when it doesn't compile

--- lua-gtk/mingw/libffi-mingw/build-libffi-mingw32.sh
+++ lua-gtk/mingw/libffi-mingw/build-libffi-mingw32.sh
@@ -4,6 +4,8 @@
 # requires quite a few installed tools.  YMMV
 #
 
+set -e
+
 DESTDIR=$(pwd)
 
 cd /tmp
@@ -17,7 +19,22 @@ aclocal
 automake --add-missing
 autoconf
 libtoolize --force
+
+# warns about using --build instead of --host.  don't.
 ./configure --host=i586-mingw32msvc
+
+# configure may fail with a message relating to "./../../config-ml.in".  Bad
+# luck, don't know how to fix that now.
+# ${multi_basedir} seems to be set badly
+
+# hint.
+#  edit config.status, go to the offending line.
+#  just above it is
+#	*" Makefile "*)
+# replace it by
+#	foo)
+# Then run ./config.status, and then make.
+
 make
 
 cp .libs/libffi.a include/*.h $DESTDIR
