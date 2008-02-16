2853b72b03cddd0471bf63bc166d21f85cdeec88
Wolfgang Oertl <wiz@luaforge.net>
2008-02-16 15:22:57 +0000

    - removal of call to set_debug_flags - would fail if compiled with   configure --disable-debug

--- lua-gtk/tests/009.lua
+++ lua-gtk/tests/009.lua
@@ -1,7 +1,7 @@
 #! /usr/bin/env lua
 require "gtk"
 
-gtk.set_debug_flags("valgrind")
+-- gtk.set_debug_flags("valgrind")
 w = gtk.window_new(gtk.GTK_WINDOW_TOPLEVEL)
 layout = w:create_pango_layout("Demo Message")
 
