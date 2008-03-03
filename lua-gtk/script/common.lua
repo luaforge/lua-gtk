4c51145241b0bbb2aa99b376e022e38a282bf540
Wolfgang Oertl <wiz@luaforge.net>
2008-03-03 20:41:09 +0000

    - changed encoding for enums

--- lua-gtk/script/common.lua
+++ lua-gtk/script/common.lua
@@ -1,3 +1,4 @@
+-- vim:sw=4:sts=4
 
 ---
 -- Format a 16 bit value into two octal bytes in high/low order.
@@ -8,40 +9,74 @@ function format_2bytes(val)
 end
 
 ---
--- Compute the representation of an ENUM in the hash table.
+-- Compute the representation of an ENUM in the hash table.  The format is
+-- described in doc/README.
 --
-function encode_enum(name, val, struct_id)
-    local s = ""
-    local t = type(val)
 
+enum_count = 0
+enum_rawdata = 0
+enum_strings = 0
+enum_negative = 0
+enum_typenr = 0
+
+function encode_byte(val)
+    return string.format("\\%03o", val)
+end
+
+function encode_enum(name, val, type_idx)
+    local s = ""
+    local extra = ""
+    local t = type(val)
+    local c = 0		-- first byte
+    local have_type = false
+
+    enum_count = enum_count + 1
+
+    -- if type_idx is given, set high bit of flag and encode it
+    if type_idx and type_idx ~= 0 then
+	extra = encode_byte(bit.band(type_idx, 255))
+	c = 0x80 + bit.rshift(type_idx, 8)
+	have_type = true
+	enum_typenr = enum_typenr + 1
+    end
+
+    -- if string, simply append the string.
     if t == "string" then
-	assert(struct_id == 0)
-	s = format_2bytes(0xfffe) .. val
+	enum_strings = enum_strings + 1
+	c = bit.bor(c, 0x40)
+	s = val
+	enum_rawdata = enum_rawdata + #val
     elseif t == "number" then
-	s = encode_enum_integer(val, struct_id)
+	-- if type_idx is not set, use some bits of the first byte
+	limit = have_type and 0 or 0x1f
+
+	if val < 0 then
+	    c = bit.bor(c, 0x20)
+	    val = -val
+	    enum_negative = enum_negative + 1
+	end
+
+	while val > limit do
+	    s = encode_byte(bit.band(val, 255)) .. s
+	    val = bit.rshift(val, 8)
+	    enum_rawdata = enum_rawdata + 1
+	end
+
+	-- or the remaining bits into the first byte.
+	c = bit.bor(c, val)
     else
 	error("unhandled type " .. type(val) .. " in encode_enum")
     end
 
-    return string.format("%s,%s", name, s)
+    enum_rawdata = enum_rawdata + 1
+    return string.format("%s,%s", name, encode_byte(c) .. extra .. s)
 end
 
--- output an integer into a string with octal bytes, high order byte first.
--- prefix with $FFFF if negative.
-function encode_enum_integer(val, struct_id)
-    local s = ""
-    local prefix = ""
-
-    -- negative values are very rare.  prefix them by $FFFF.
-    if val < 0 then
-	val = -val
-	prefix = format_2bytes(0xffff)
-    end
-
-    while val > 0 do
-	s = string.format("\\%03o", bit.band(val, 255)) .. s
-	val = bit.rshift(val, 8)
-    end
-    return prefix .. format_2bytes(struct_id) .. s
+function enum_statistics()
+    info_num("Enum Count", enum_count)
+    info_num("Enum raw data bytes", enum_rawdata)
+    info_num("Enum string count", enum_strings)
+    info_num("Enum negative numbers", enum_negative)
+    info_num("Enums with type_idx", enum_typenr)
 end
 
