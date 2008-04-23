69569376954bce5b9d34482b5cad285f257f8343
Wolfgang Oertl <wiz@luaforge.net>
2008-04-23 15:14:31 +0000

    - modified to reflect the changed API for void* wrappers

--- lua-gtk/tests/027-closure.lua
+++ lua-gtk/tests/027-closure.lua
@@ -9,21 +9,62 @@
 require "gtk"
 
 function compare_func(a, b)
-    print("keyfunc", a, b)
+    a = a.value
+    b = b.value
     if a == b then return 0 end
     return a < b and -1 or 1
 end
 
+seen = {}
+
+-- The iterator function must return a boolean: false to keep going, true to
+-- stop.  If it returns something else, the foreach function aborts with an
+-- error.
 function traverse_func(key, value, user_data)
-    print("traverse_func", key, value, user_data)
-    return nil
+    key = key.value
+    seen[key] = (seen[key] or 0) + 1
+    return user_data.value
+end
+
+-- Destroy functions.  They receive the value, not the wrapper, and therefore
+-- cannot do anything about the wrapper!
+function key_destroy(key)
+    key:destroy()
+end
+
+function value_destroy(value)
+    value:destroy()
 end
 
 -- has to create a closure
-t = gtk.g_tree_new(compare_func)
+t = gtk.g_tree_new_full(compare_func, nil, key_destroy, value_destroy)
 
+-- add a node to the tree.  parameter as "gpointer", i.e. a wrapper is created
 t:insert("a", "value a")
+
+-- add a second node; has to call the compare_func.  This fails...
 t:insert("b", "value b")
 
-t:foreach(traverse_func, 99)
+-- when using an iterator that doesn't return boolean, an error must happen.
+rc, msg = pcall(t.foreach, t, traverse_func, nil)
+assert(rc == false)
+
+-- a "good" iterator function must work.
+v = gtk.void_ptr(false)
+t:foreach(traverse_func, v)
+v:destroy() -- should happen automatically??
+
+v = gtk.void_ptr(true)
+t:foreach(traverse_func, v)
+v:destroy()
+
+assert(seen.a == 3)
+assert(seen.b == 1)
+
+t:destroy()
+
+collectgarbage("collect")
+collectgarbage("collect")
+
+-- show garbage...
 
