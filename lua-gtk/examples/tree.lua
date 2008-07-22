642ed4fed359993f2e5b7ee08dbf1805fd73e422
Wolfgang Oertl <wiz@luaforge.net>
2008-07-22 15:26:22 +0000

    - closures now have to be created explicitely

--- lua-gtk/examples/tree.lua
+++ lua-gtk/examples/tree.lua
@@ -43,10 +43,12 @@ end
 function show_selection_2(btn, mainwin)
     local tv = mainwin.tree_view
     local sel = tv:get_selection()
-    sel:selected_foreach(function(model, path, iter, data)
+    sel:selected_foreach(gtk.closure(function(model, path, iter, data)
 	local s = model:get_value(iter, 2)
 	print(s)
-    end, nil)
+	-- demonstrate that the active closure won't be collected
+	collectgarbage "collect"
+    end), nil)
 end
 
 function MainWin.new()
