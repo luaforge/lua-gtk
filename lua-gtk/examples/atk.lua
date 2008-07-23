3a85a7493c6e65870e4ddce90b7864abc58749db
Wolfgang Oertl <wiz@luaforge.net>
2008-07-23 11:14:47 +0000

    New example to test Atk functions (not much yet)

---
+++ lua-gtk/examples/atk.lua
@@ -0,0 +1,22 @@
+#! /usr/bin/env lua
+-- vim:sw=4:sts=4
+require "gtk"
+
+-- Experiment with ATK.  Not yet useful
+
+function build_ui()
+    w = gtk.window_new(gtk.GTK_WINDOW_TOPLEVEL)
+    w:connect('destroy', gtk.main_quit)
+    w:set_title("ATK experiment")
+    a = w:get_accessible()
+    w:show_all()
+
+    print(w, a)
+    print("name", a:atk_object_get_name())
+    print("description", a:atk_object_get_description())
+    print("parent", a:get_parent())
+end
+
+build_ui()
+gtk.main()
+
