0d3b85f5cc8645f516fd37c25f374560900311f0
Wolfgang Oertl <wiz@luaforge.net>
2008-08-25 17:34:35 +0000

    - detection of libffi if it has no pkg-config file

--- lua-gtk/script/config.linux.lua
+++ lua-gtk/script/config.linux.lua
@@ -4,14 +4,33 @@ if not use_dynlink then
     gtk_libs = pkg_config("--libs", "gtk+-2.0")
 end
 
-if pkg_config_exists "libffi" then
-    libffi_version, libffi_lib, libffi_inc = pkg_config("--modversion",
-	"--libs", "--cflags", "libffi")
-    libffi_inc = libffi_inc or ""
-else
-    cfg_err("No pkg-config for libffi.")
-    -- XXX try to autodetect the location
+function detect_ffi()
+
+    if false and pkg_config_exists "libffi" then
+	libffi_version, libffi_lib, libffi_inc = pkg_config("--modversion",
+	    "--libs", "--cflags", "libffi")
+	libffi_inc = libffi_inc or ""
+	return
+    end
+
+    -- no pkg-config for libffi found (which was added for libffi5)
+    local f = find_file("libffi_pic.a", "/usr/" .. arch_cpu .. "-linux-gnu/lib",
+	"/usr/lib", "/usr/local/lib")
+    if not f then
+	f = find_file("libffi.so", "/usr/" .. arch_cpu .. "-linux-gnu/lib",
+	    "/usr/lib", "/usr/local/lib")
+	if not f then
+	    cfg_err("libffi.so not found.")
+	    return
+	end
+    end
+
+    libffi_version = "4"	-- just a guess
+    libffi_lib = f
+    libffi_inc = ""		-- hopefully in the search path
 end
+
+detect_ffi()
 
 -- extra include for libffi when cross compiling
 if host_arch ~= arch then
