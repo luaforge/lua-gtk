0a05df4fa067bdcc9c8689afbd85f4cab3d400a4
Wolfgang Oertl <wiz@luaforge.net>
2008-12-30 16:22:01 +0000

    - removal of messages to stdout - changes due to modularization

--- lua-gtk/tests/014-iconv.lua
+++ lua-gtk/tests/014-iconv.lua
@@ -1,30 +1,29 @@
 #! /usr/bin/env lua
-require "gtk"
+-- vim:sw=4:sts=4
 
--- call g_convert functions; this also tests how char** arguments
--- are handled; right now not very well.
+require "glib"
 
+-- test g_convert and g_iconv; the latter has an override.
 
 latin1_string = "Teststring - öäü ÖÄÜ ß ENDE AAAAAAAAAAAAAAA ENDE2"
+assert(#latin1_string == 49)
 
 -- g_convert
-s, read, written, err = gtk.g_convert(latin1_string, -1, "UTF8", "ISO-8859-1",
-	0, 0, nil)
-
-print("Ergebnis von g_convert", s, read, written, err)
-
+result1, read, written, err = glib.convert(latin1_string, -1, "UTF8",
+    "ISO-8859-1", 0, 0, nil)
+assert(#result1 == 56)
+assert(read == 49)
+assert(written == #result1)
+assert(err == nil)
 
 -- set up conversion to utf8
-conv = gtk.g_iconv_open("UTF8", "ISO-8859-1")
-
--- val = latin1_string
+conv = glib.iconv_open("UTF8", "ISO-8859-1")
+assert(conv)
 
 -- This call will modify obuf - which is usually not OK, because in Lua
 -- strings are immutable - but anyway works somewhat.
-
-a, b, c = gtk.g_iconv(conv, latin1_string)
-
-print("ERG:", a, #b, #c, b, c)
--- print("eingabestring - sollte nicht gut lesbar sein, weil nicht utf8:\n", val)
--- print("obuf", obuf)
+rc, result2, rest = glib.iconv(conv, latin1_string)
+assert(rc == 0)
+assert(result2 == result1)
+assert(#rest == 0)
 
