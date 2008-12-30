909b0423d9efd0eeba8e613504fbc19bde1a0af6
Wolfgang Oertl <wiz@luaforge.net>
2008-12-30 16:12:27 +0000

    - reformatting of the code - gtk.main_quit can be connected to directly - don't use the "append1" override anymore (removed)

--- lua-gtk/examples/combobox.lua
+++ lua-gtk/examples/combobox.lua
@@ -1,4 +1,5 @@
 #! /usr/bin/env lua
+-- vim:sw=4:sts=4
 
 -- demonstration of a tree view
 
@@ -9,50 +10,60 @@ MainWin.__index = MainWin
 
 function MainWin.new()
 
-	local self = {}
-	setmetatable(self, MainWin)
+    local self = {}
+    setmetatable(self, MainWin)
 
-	-- create visible widgets
-	self.w = gtk.window_new(gtk.GTK_WINDOW_TOPLEVEL)
-	self.w:connect('destroy', function() gtk.main_quit() end)
-	self.w:set_default_size(200, 50)
-	self.w:set_title("ComboBox Demo")
+    -- create visible widgets
+    self.w = gtk.window_new(gtk.WINDOW_TOPLEVEL)
+    self.w:connect('destroy', gtk.main_quit)
+    self.w:set_default_size(200, 50)
+    self.w:set_title("ComboBox Demo")
 
-	local vbox = gtk.vbox_new(false, 3)
-	self.w:add(vbox)
+    local vbox = gtk.vbox_new(false, 3)
+    self.w:add(vbox)
 
-	self.combobox = gtk.combo_box_new()
-	vbox:add(self.combobox)
+    self.combobox = gtk.combo_box_new()
+    vbox:add(self.combobox)
 
-	-- create store
-	self.store = gtk.tree_store_new(4, gtk.G_TYPE_INT, gtk.G_TYPE_STRING,
-		gtk.G_TYPE_STRING, gtk.G_TYPE_STRING)
-	self.combobox:set_model(self.store)
+    -- create store
+    self.store = gtk.tree_store_new(4, glib.TYPE_INT, glib.TYPE_STRING,
+	    glib.TYPE_STRING, glib.TYPE_STRING)
+    self.combobox:set_model(self.store)
 
-	-- define visible columns
-	local r = gtk.cell_renderer_text_new()
-	self.combobox:pack_start(r, false)
-	self.combobox:set_attributes(r, 'text', 1, 'foreground', 3, nil)
+    -- define visible columns
+    local r = gtk.cell_renderer_text_new()
+    self.combobox:pack_start(r, false)
+    self.combobox:set_attributes(r, 'text', 1, 'foreground', 3, nil)
 
-	r = gtk.cell_renderer_text_new()
-	self.combobox:pack_start(r, false)
-	self.combobox:set_attributes(r, 'text', 2, nil)
+    r = gtk.cell_renderer_text_new()
+    self.combobox:pack_start(r, false)
+    self.combobox:set_attributes(r, 'text', 2, nil)
 
 
-	-- add some items
-	local iter1, iter2 = gtk.new "GtkTreeIter", gtk.new "GtkTreeIter"
-	for i = 1, 10 do
-		self.store:append1(iter1, nil, i, "Item " .. i,
-			"Info " .. i, "green")
-		for j = 1, 10 do
-			self.store:append1(iter2, iter1, i*10+j-1,
-				"Subitem " .. j, "Subinfo " .. j, "blue")
-		end
-
+    -- add some items
+    local iter1, iter2 = gtk.new "GtkTreeIter", gtk.new "GtkTreeIter"
+    for i = 1, 10 do
+	self.store:append(iter1, nil)
+	self.store:set(iter1,
+	    0, i,
+	    1, "Item " .. i,
+	    2, "Info " .. i,
+	    3, "green",
+	    -1)
+	for j = 1, 10 do
+	    self.store:append(iter2, iter1)
+	    self.store:set(iter2,
+		0, i*10+j-1,
+		1, "Subitem " .. j,
+		2, "Subinfo " .. j,
+		3, "blue",
+		-1)
 	end
 
-	self.w:show_all()
-	return self
+    end
+
+    self.w:show_all()
+    return self
 
 end
 
