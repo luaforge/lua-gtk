64baaf022a81c671d4aee0eb3f8c48aeb6bcbc5a
Wolfgang Oertl <wiz@luaforge.net>
2008-12-30 16:11:31 +0000

    - closures must be used explicitely - modularization, so some functions are in glib now

--- lua-gtk/examples/clock.lua
+++ lua-gtk/examples/clock.lua
@@ -4,21 +4,33 @@
 
 require "gtk"
 
-win = gtk.window_new(gtk.GTK_WINDOW_TOPLEVEL)
+win = gtk.window_new(gtk.WINDOW_TOPLEVEL)
 win:set_size_request(400, 300)
 win:set_title("Clock")
 win:connect('destroy', gtk.main_quit)
 
-function onTimeout(lbl)
+function onTimeout(ar)
+    local lbl = ar.lbl
     lbl:set_text(os.date('%H:%M:%S'))
     return true
 end
 
-lbl = gtk.label_new('')
-onTimeout(lbl)
-timer = gtk.g_timeout_add(1000, onTimeout, lbl)
+lbl = gtk.label_new ""
+onTimeoutClosure = gnome.closure(onTimeout)
+onTimeoutArg = gnome.void_ptr{ lbl=lbl }
+onTimeout(onTimeoutArg)
+timer = glib.timeout_add(1000, onTimeoutClosure, onTimeoutArg)
+collectgarbage "collect"
 
 win:add(lbl)
 win:show_all()
 gtk.main()
+
+onTimeoutClosure = nil
+onTimeoutArg = nil
+lbl = nil
+win = nil
+collectgarbage "collect"
+print(gnome.get_vwrapper_count())
+
 
