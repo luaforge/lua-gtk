9b5ce96f6520044c27946fd04f23db82c2a018a7
Wolfgang Oertl <wiz@luaforge.net>
2008-12-30 15:33:49 +0000

    Setup of the build process

---
+++ lua-gtk/src/cairo/spec.lua
@@ -0,0 +1,34 @@
+-- vim:sw=4:sts=4
+
+name = "Cairo"
+pkg_config_name = "cairo"
+
+include_dirs = { "cairo" }
+
+libraries = {}
+libraries.linux = { "/usr/lib/libcairo.so" }
+libraries.win32 = { "libcairo-2.dll" }
+
+includes = {}
+includes.all = {
+    "<cairo.h>",
+}
+
+function_flags = {
+    cairo_status_to_string = CONST_CHAR_PTR,
+    cairo_version_string = CONST_CHAR_PTR,
+}
+
+moddep = {
+    "glib",
+}
+
+-- extra settings for the module_info structure
+module_info = {
+
+    prefix_func = '"cairo_"',
+    prefix_constant = '"CAIRO_"',
+    prefix_type = '"cairo_"',
+    depends = '""',
+}
+
