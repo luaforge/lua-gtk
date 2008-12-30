741b43aa7a0efd211272aded283e90d7de23f24c
Wolfgang Oertl <wiz@luaforge.net>
2008-12-30 16:24:35 +0000

    - more tests to box various data types; access tables directly

--- lua-gtk/tests/028-boxed.lua
+++ lua-gtk/tests/028-boxed.lua
@@ -4,14 +4,15 @@
 require "gtk"
 
 -- create a list store with a column of this boxed type.
-store = gtk.list_store_new(1, gtk.boxed_type)
-iter = gtk.new "GtkTreeIter"
+store = gtk.list_store_new(1, gnome.boxed_type)
+iter = gtk.new "TreeIter"
 
 -- store a value into that.  currently, an explicit cast to boxed is required.
-tbl = { _type="boxed" }
+tbl = { _type="boxed", name="One" }
 store:insert_with_values(iter, 0,
     0, tbl,
     -1)
+collectgarbage "collect"
 
 -- retrieve that value again.
 if true then
@@ -33,9 +34,26 @@ store:set_value(iter, 0, nil)
 val = store:get_value(iter, 0)
 assert(val == nil)
 
--- explicit boxing
-val = gtk.make_boxed_value "The boxed string"
+-- explicit boxing of a string
+val = gnome.box "The boxed string"
 assert(type(val) == "userdata")
-val2 = gtk.get_boxed_value(val)
-assert(val2 == "The boxed string")
+assert(val.value == "The boxed string")
+store:set_value(iter, 0, val)
+val2 = store:get_value(iter, 0)
+assert(type(val2) == "string")
+assert(val2 == val.value)
+
+-- same for a table
+val = gnome.box { field1=10, field2=20, field3=30 }
+assert(val.field1 == 10)
+assert(val.field2 == 20)
+assert(val.value.field3 == 30)
+val.field4 = 40
+assert(val.field4 == 40)
+
+-- All GBoxed values must be freed properly by garbage collection; check that.
+val = nil
+store = nil
+collectgarbage "collect"
+assert(gnome.box_debug() == 0)
 
