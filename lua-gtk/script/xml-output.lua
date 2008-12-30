76b3b7b3a6de0df39b8cb0dd984dbee568a070e4
Wolfgang Oertl <wiz@luaforge.net>
2008-12-30 09:28:20 +0000

    - support non-native types, i.e. store which module handles a non-native type   for auto-loading - various changes for renamed include files, changed structures etc. - support arrays, i.e. storing bounds of array types - removal of the char_ptr_handling code; replacedd by function flags - LUA2FFI and FFI2LUA constants combined into CONV constants; same goes for   LUA2STRUCT/STRUCT2LUA to STRUCTCONV - store hashes of names of fundamental types - write a C files (generated.c) that has the complete module_info structure

--- lua-gtk/script/xml-output.lua
+++ lua-gtk/script/xml-output.lua
@@ -10,34 +10,47 @@ max_type_id = 0   -- last type_id in use
 struct_count = 0    -- structs or unions
 max_struct_size = 0
 max_struct_elems = 0
+headerstrings = {}	-- to be written into the .h file
+mi_strings = {}		-- to build the "struct modinfo"
+
+-- Add another definition to the header
+local function header(fmt, ...)
+    headerstrings[#headerstrings + 1] = string.format(fmt, ...)
+end
+
+local function modinfo(fmt, ...)
+    mi_strings[#mi_strings + 1] = string.format(fmt, ...)
+end
 
 ---
--- Generate a sorted list of ENUMs suitable for postprocessing with a hashing
--- tool.
+-- Generate a sorted list of constants suitable for postprocessing with a
+-- hashing tool.  Note that constants with a type that is not otherwise
+-- used is omitted.
 --
 -- Each entry consist of the name, ",", the 16 bit number of the structure
--- number (describing this ENUM), and the actual value in a low to high byte
--- order.  Only as many bytes are output as are required to represent the given
--- value.  The value zero has no bytes.
+-- number (describing this constant), and the actual value in a low to high
+-- byte order.  Only as many bytes are output as are required to represent the
+-- given value.  The value zero has no bytes.
 --
 -- Call this function _after_ output_structs, because there the type_ids
 -- are assigned.
 --
 -- @param ofname Name of the output file
 --
-function output_enums(ofname)
+function output_constants(ofname)
     local keys, t, ofile, s, enum, val, prefix = {}
 
     for k, enum in pairs(xml.enum_values) do
 	t = typedefs[enum.context]
 	assert(t, "Unknown context (structure) " .. tostring(enum.context)
 	    .. " for enum " .. k)
-	if t.in_use or t.enum_redirect then
+	if (t.in_use or t.enum_redirect) and not t.no_good then
 	    keys[#keys + 1] = k
 	end
     end
     table.sort(keys)
 
+    header("extern const struct hash_info hash_info_constants;")
     ofile = io.open(ofname, "w")
 
     for i, name in pairs(keys) do
@@ -79,7 +92,7 @@ function store_string(bufname, s, omit_nul)
 	    tbl={},		    -- [string] => offset
 	    buf={},		    -- [i] = string #i
 	    offsets={},	    -- [i] => offset of this entry (for debugging)
-	    next_ofs=0	    -- offset of next string to be added
+	    next_ofs=1	    -- offset of next string to be added
 	}
 	string_buf[bufname] = buf
     end
@@ -108,11 +121,13 @@ end
 -- more characters.
 --
 function output_strings(ofile)
-    local maxlen
+    local maxlen, sig
     for name, buf in pairs(string_buf) do
 	maxlen = 0
-	ofile:write(string.format("const %schar type_strings_%s[] = \n",
-	    name == "proto" and "unsigned " or "", name));
+	sig = string.format("const %schar %sstrings_%s[]",
+	    name == "proto" and "unsigned " or "", config.prefix, name)
+	header("extern %s;", sig)
+	ofile:write(sig .. " = \"\\0\"\n")
 	for i, s in ipairs(buf.buf) do
 	    ofile:write(string.format(" \"%s\" /* %d */\n",
 		string.gsub(s, "[^a-zA-Z0-9_. *]",
@@ -196,6 +211,43 @@ function output_one_struct(ofile, tp, struct_name)
     st.elem_count = elem_start - st.elem_start
 end
 
+file_to_module = {}
+
+---
+-- When a type is non-native, look through the aggregated include list of
+-- the other modules.  If the path of the file that defines the given type
+-- can be found, this is the module that handles the type.
+--
+-- If no module is responsible, no automatic loading can happen when the type
+-- is accessed, and an error will happen, unless the user loads a third-party
+-- module that handles it.
+--
+function _find_non_native_module(t)
+    local ofs, fname
+
+    if not t.file_id then return false end
+    ofs = file_to_module[t.file_id]
+    if ofs then return ofs end
+
+    fname = xml.filelist[t.file_id]
+    ofs = false
+    for path, modname in pairs(non_native_includes) do
+	if string.find(fname, path, 1, true) then
+	    ofs = store_string("types", modname)	-- was "modules"
+	    break
+	end
+    end
+
+    if not ofs then
+	print("Warning: no module found for type " .. tostring(t.full_name)
+	    .. " from " .. fname)
+    end
+
+    -- found (or not, ofs == false in this case).
+    file_to_module[t.file_id] = ofs
+    return ofs
+end
+
 ---
 -- Write a C file with the structure information, suitable for compilation
 -- and linking to the library.
@@ -207,69 +259,145 @@ function output_types(ofname)
 
     local keys = typedefs_sorted
     local name2id = typedefs_name2id
+    local types_native, types_native_strings, types_foreign = 0, 0, 0
+    local array_list = {}
 
     ofile = io.open(ofname, "w")
-    ofile:write("#include \"luagtk.h\"\n")
+    ofile:write("#include \"common.h\"\n")
 
-    -- generate the list of struct/union elements
-    -- fields: name_ofs, bit_offset, bit_len, ffi_type_id, type_detail
+    -- Generate the list of struct/union elements for types that are
+    -- "native" to this module.
     elem_start = 0
-    ofile:write("const struct struct_elem elem_list[] = {\n")
+    header("extern const struct struct_elem %selem_list[];", config.prefix)
+    ofile:write(string.format("const struct struct_elem %selem_list[] = {\n",
+	config.prefix))
     for i, full_name in ipairs(keys) do
 	t = typedefs[name2id[full_name]]
 	assert(t)
 	assert(t.extended_name)
-	t.name_ofs = store_string("types", t.extended_name)
 
-	if t.detail and t.detail.struct then
-	    output_one_struct(ofile, t.detail, full_name)
+	-- do not store anything for non-native types
+	if t.is_native then
+	    t.name_ofs = store_string("types", t.extended_name)
+	    if t.detail and t.detail.struct then
+		output_one_struct(ofile, t.detail, full_name)
+	    end
 	end
     end
     ofile:write("};\n\n")
 
     -- type_list.
-    ofile:write("const struct type_info type_list[] = {\n"
-	.. " { 0, 0 }, /* placeholder for undefined structures */\n")
+    header("extern const union type_info %stype_list[];", config.prefix)
+    ofile:write(string.format("const union type_info %stype_list[] = {\n"
+	.. " { nn: { %d, 0, 0, 0 } }, /* placeholder for undefined types */\n",
+	config.prefix, 0))
+
     for i, full_name in ipairs(keys) do
 	t = typedefs[name2id[full_name]]
-	assert(t.name_ofs, "no name_ofs defined for type " .. full_name)
 	assert(t.type_idx, "no type_id defined for type " .. full_name)
 --	assert(t.type_idx == i, "type ID mismatch: " .. t.type_idx .. " vs "
 --	    .. i .. " for type " .. full_name)
 
 	local detail = t.detail
 
-	-- common elements
+	-- integrity checks
 	local fu = types.ffi_type_map[t.fid]
 	assert(t.pointer == fu.pointer, "pointer mismatch for " .. t.full_name
 	    .. ": " .. t.pointer .. " vs " .. fu.pointer .. ", fu=" .. fu.name)
-	s = string.format(" { %d, %d, %d", t.fid, t.const and 1 or 0,
-	    t.name_ofs)
+	if t.is_native then
+	    assert(t.name_ofs, "no name_ofs defined for type " .. full_name)
+	end
 
-	if detail and detail.struct then
+	if not t.is_native then
+	    assert(not t.name_ofs, "name_ofs defined for non-native type "
+		.. full_name)
+	    local hash_value = gnomedev.compute_hash(full_name)
+	    local name_ofs = _find_non_native_module(t)
+	    local name_is_module
+
+	    if name_ofs then
+		name_is_module = 1
+	    else
+		-- no known module; need to store the type's name itself.
+		name_ofs = store_string("types", t.extended_name) or 0
+		name_is_module = 0
+	    end
+	    s = string.format(" { nn: { %d, %d, 0, %d, 0x%08x } }",
+		0, name_is_module, name_ofs, hash_value)
+	elseif t.fname == "func" then
+	    s = string.format(" { fu: { %d, %d, %d, %d, 0, %d } }",
+		2, t.fid, t.name_ofs, t.indir, detail.proto_ofs)
+	elseif detail and detail.struct then
 	    st = detail.struct
 	    assert(st)
 	    local struct_size = (st.size or 0)/8
-	    s = s .. string.format(", { st: { %d, %d, %d } } }, /* %d: %s %s */\n",
-		struct_size, st.elem_start, st.elem_count,
-		t.type_idx, t.fname or "", full_name)
+	    s = string.format(
+		" { st: { %d, %d, %d, %d, 0, %d, %d, %d, %d, %d } }",
+		1, t.fid, t.name_ofs, t.indir, t.const and 1 or 0,
+		t.array and 1 or 0,
+		struct_size, st.elem_start, st.elem_count)
 	    max_struct_size = math.max(max_struct_size, struct_size)
 	    max_struct_elems = math.max(max_struct_elems, st.elem_count)
-	elseif t.fname == "func" then
-	    s = s .. string.format(", { fu: { %d } } }, /* %d: func %s */\n",
-		detail.proto_ofs, t.type_idx, full_name)
+
+	    if t.array then
+		_add_array(array_list, t)
+	    end
 	else
-	    s = s .. string.format(" }, /* %d: %s */\n", t.type_idx, full_name)
+	    -- no structure.... must be a fundamental type.
+	    s = string.format(
+		" { st: { %d, %d, %d, %d, 0, %d, %d } }",
+		3, t.fid, t.name_ofs, t.indir, t.const and 1 or 0,
+		t.array and 1 or 0)
+	    if t.array then
+		_add_array(array_list, t)
+	    end
+	end
+
+	s = s .. string.format(", /* %d: %s %s */\n",
+	    t.type_idx, t.fname or "", full_name)
+
+	if t.is_native then
+	    types_native = types_native + 1
+	    types_native_strings = types_native_strings + #t.extended_name
+	else
+	    types_foreign = types_foreign + 1
 	end
 
 	ofile:write(s)
     end
 
-    ofile:write("};\nconst int type_count = " .. #keys .. ";\n\n")
+    ofile:write(string.format("};\n\n"))
 
+    -- write that later into the .h file
+    -- header("#define TYPE_COUNT %d", #keys)
+    config.type_count = #keys
+
+    -- List of array types and their dimensions
+    array_list[#array_list + 1] = " { 0 }\n";
+    header("extern const struct array_info %sarray_list[];", config.prefix)
+    ofile:write(string.format(
+	"const struct array_info %sarray_list[] = {\n%s};\n\n",
+	config.prefix, table.concat(array_list)))
 
     output_strings(ofile)
+--    if not string_buf.modules then
+--	header("#define glib_strings_modules NULL")
+--    end
     ofile:close()
+
+    print "Type statistics:"
+    print(string.format("native types=%d, strlen=%d", types_native,
+	types_native_strings))
+    print(string.format("foreign types=%d", types_foreign))
+end
+
+---
+-- The type t is an array.  Append another line to the array_list.
+function _add_array(array_list, t)
+    -- print("array list", t.type_idx, t.full_name, t.array[1], t.array[2])
+    array_list[#array_list + 1] = string.format(
+	" { %d, { %d, %d } }, /* %s */\n",
+	t.type_idx, t.array[1], t.array[2] or 0, t.full_name)
 end
 
 ---
@@ -283,6 +411,7 @@ function output_functions(ofname)
     local ofile, pos, s
 
     -- write that list
+    header("extern const struct hash_info hash_info_functions;")
     ofile = io.open(ofname, "w")
     for i, k in ipairs(function_list) do
 	s = _function_signature(k)
@@ -320,15 +449,16 @@ function _type_name_add(s)
 end
 
 function _type_names_flush(ofile)
-    s = "const char ffi_type_names[] = \n"
-	.. table.concat(_type_names, "\n")
-	.. ";\n\n";
+    header("const char %sffi_type_names[];", config.prefix)
+    s = string.format("const char %sffi_type_names[] = \n%s;\n\n",
+	config.prefix, table.concat(_type_names, "\n"))
     ofile:write(s)
 end
 
 ---
 -- Write the list of fundamental types to an output file, which can be compiled
--- as C code.
+-- as C code.  This is only required for the core module, which handles
+-- all accesses to them.
 --
 -- @param ofname Name of the file to write to, which will be overwritten if
 --  it exists.
@@ -336,32 +466,27 @@ end
 function output_fundamental_types(ofname)
     local ofile, ffitype, type_code, ofs
 
-    assert(char_ptr_second)
+    assert(config.is_core)
     ofile = io.open(ofname, "w")
     ofs = _type_name_add("INVALID")
+
     ofile:write(string.format("struct ffi_type_map_t ffi_type_map[] = {\n"
 	.. "  { %d, },\n", ofs))
 
     for i, v in ipairs(types.ffi_type_map) do
-	ffitype = types.fundamental_to_ffi(v) or { nil, 0, nil, nil, nil, nil }
-
-	-- Here the second char* entry gets the "4" flag to mark it as const.
-	-- This is then used in src/types.c:ffi2lua_char_ptr.
-	-- FFI_CHAR_PTR_CONST
-	local flags = (i == char_ptr_second) and 1 or 0
-
+	ffitype = types.fundamental_to_ffi(v) or {}
 	ofs = _type_name_add(v.name)
-	ofile:write(string.format("  { %d, %d, %d, %d, %s, %s, %s, %s, %s }, "
-	    .. "/* %d %s */\n",
+	ofile:write(string.format("  { %d, %d, %d, %s, %s, "
+	    .. "%s }, /* %d %s */\n",
 	    ofs,		-- name_ofs
 	    v.bit_len or 0,	-- bit_len
 	    v.pointer,		-- indirections
-	    ffitype[2] + flags,	-- flags
-	    ffitype[3] and "LUA2FFI_" .. string.upper(ffitype[3]) or 0,
-	    ffitype[4] and "FFI2LUA_" .. string.upper(ffitype[4]) or 0,
-	    ffitype[5] and "LUA2STRUCT_" .. string.upper(ffitype[5]) or 0,
-	    ffitype[6] and "STRUCT2LUA_" .. string.upper(ffitype[6]) or 0,
-	    ffitype[1] and "LUAGTK_FFI_TYPE_" .. string.upper(ffitype[1]) or 0,
+	    ffitype[2] and "CONV_" .. string.upper(ffitype[2]) or 0,
+	    ffitype[3] and "STRUCTCONV_" .. string.upper(ffitype[3]) or 0,
+
+
+	    ffitype[1] and "LUAGTK_FFI_TYPE_" .. string.upper(ffitype[1])
+		or 0,
 	    i, v.name
 	))
     end
@@ -370,26 +495,50 @@ function output_fundamental_types(ofname)
     -- in the ffi_type_map table.
     ofile:write(string.format("};\nconst int ffi_type_count = %d;\n\n",
 	#types.ffi_type_map + 1))
+
     _type_names_flush(ofile)
     ofile:close()
 end
 
+function output_fundamental_hash(ofname)
+    local res, ofile = {}
+
+    assert(not config.is_core)
+
+    for i, v in ipairs(types.ffi_type_map) do
+	res[#res + 1] = string.format("  0x%08x, /* %s */\n",
+	    gnomedev.compute_hash(v.name), v.name)
+    end
+
+    ofile = io.open(ofname, "w")
+    header("extern const unsigned int %sfundamental_hash[];", config.prefix)
+    ofile:write(string.format("const unsigned int %sfundamental_hash[] = {\n",
+	config.prefix))
+    ofile:write(table.concat(res))
+    ofile:write("};\n\n")
+    ofile:close()
+
+    -- header("#define FUNDAMENTAL_COUNT %d", #res)
+    config.fundamental_count = #res
+end
 
 ---
--- Write a sorted list of globals.  Maybe not required, but anyway, doesn't
--- hurt.  It is not used yet.
+-- Write a sorted list of globals, so that they can be accessed.
 --
 function output_globals(ofname)
     local keys, ofile, gl, tp = {}
     local count = 0
 
     for k, v in pairs(xml.globals) do
-	keys[#keys + 1] = v.name
+	if v.is_native then
+	    keys[#keys + 1] = v.name
+	end
     end
     table.sort(keys)
 
     ofile = io.open(ofname, "w")
-    ofile:write "const char luagtk_globals[] =\n"
+    header("extern const char %sglobals[];", config.prefix)
+    ofile:write(string.format("const char %sglobals[] =\n", config.prefix))
     for i, name in pairs(keys) do
 	gl = xml.globals[name]
 	tp = types.resolve_type(gl.type)
@@ -404,6 +553,78 @@ function output_globals(ofname)
     ofile:close()
 end
 
+function output_init()
+    header('#include "module.h"')
+end
+
+---
+-- Write a C file with some defines needed to compile
+--
+function output_code(ofname)
+    -- not required for the core module; it has no module info.
+    if config.is_core then return end
+    local ofile = io.open(ofname, "w")
+    -- to save typing in modules/init.c; in the file generated by
+    -- make-link.lua, and only if RUNTIME_LINKING is defined.
+    if config.runtime_linking then
+	header("extern const char %sdynlink_names[];", config.prefix)
+    end
+    _generate_module_info()
+    ofile:write(table.concat(headerstrings, "\n") .. "\n")
+    ofile:close()
+end
+
+---
+-- Build the "module_info" structure
+--
+function _generate_module_info()
+    local modname = config.module
+
+    -- declare hook functions
+    for k, v in pairs(config.lib.module_info or {}) do
+	if k == 'call_hook' then
+	    header('void %s(lua_State *L, struct func_info *fi);', v)
+	elseif k == 'allocate_object' then
+	    header('void *%s(cmi mi, lua_State *L, typespec_t ts, int count, '
+		.. 'int *flags);', v)
+	elseif k == 'overrides' then
+	    header('extern const luaL_reg %s[];', v)
+	end
+    end
+
+    
+    header("")
+    header("struct module_info modinfo_%s = {", modname)
+    header('    name: "%s",', modname)
+    header('    type_list: %s_type_list,', modname)
+    header('    elem_list: %s_elem_list,', modname)
+    header('    type_count: %d,', config.type_count)    -- set in output_types
+    header('    fundamental_hash: %s_fundamental_hash,', modname)
+    header('    fundamental_count: %d,', config.fundamental_count)
+    header('    type_strings_elem: %s_strings_elem,', modname)
+    header('    prototypes: %s_strings_proto,', modname)
+    header('    type_names: %s_strings_types,', modname)
+    header('    globals: %s_globals,', modname)
+    header('    array_list: %s_array_list,', modname)
+    header('    hash_functions: &hash_info_functions,')
+    header('    hash_constants: &hash_info_constants,')
+    header('    methods: module_methods,')
+    for k, v in pairs(config.lib.module_info or {}) do
+	header('    %s: %s,', k, v)
+    end
+    header('    dynlink: {')
+    if config.libraries then
+	header('      dll_list: "%s\\0",',
+	    table.concat(config.libraries, "\\0"))
+    end
+    if config.runtime_linking then
+	header('      dynlink_names: %s_dynlink_names,', modname)
+	header('      dynlink_table: %s_dynlink_table,', modname)
+    end
+    header('    },')
+    header('};')
+    header("struct module_info *thismodule = &modinfo_%s;", modname)
+end
 
 -- Output some maximum values.  This is useful to decide how many bits the
 -- various fields of the structures must have.
