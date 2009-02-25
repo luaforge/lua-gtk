985a077e16221ec4821d6ee11b842fe1a49f48de
Wolfgang Oertl <wiz@luaforge.net>
2009-02-25 14:05:39 +0000

    - clutter types now have the "Clutter" prefix prepended automatically - glib loaded automatically - set the title of the window

--- lua-gtk/src/clutter/test001.lua
+++ lua-gtk/src/clutter/test001.lua
@@ -2,16 +2,16 @@
 -- vim:sw=4:sts=4
 
 require 'clutter'
-require 'glib'
 
 clutter.init(0, nil)
 
 stage = clutter.stage_get_default()
 stage:set_size(300, 300)
-print(stage)
+stage:set_title"Clutter Demo"
+
 
 -- define a red, somewhat transparent color
-red = clutter.new "ClutterColor"
+red = clutter.new "Color"
 clutter.color_parse("#ff000088", red)
 
 -- add a red rectangle
@@ -20,13 +20,14 @@ actor:set_size(100, 30)
 actor:set_position(150, 130)
 stage:add_actor(actor)
 
--- and another
+-- and another rectangle
 actor = clutter.rectangle_new_with_color(red)
 actor:set_size(50, 50)
-actor:set_position(130, 110)
+actor:set_position(130, 100)
 stage:add_actor(actor)
 
 stage:show_all()
+
 
 clutter.main()
 
