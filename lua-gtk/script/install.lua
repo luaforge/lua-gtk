bb18a232b8faaea9ffc82ae82b3e727a8d2735bb
Wolfgang Oertl <wiz@luaforge.net>
2009-05-20 18:54:54 +0000

    - install all available libraries, not just gtk.so

--- lua-gtk/script/install.lua
+++ lua-gtk/script/install.lua
@@ -1,8 +1,9 @@
 #! /usr/bin/env lua
 -- vim:sw=4:sts=4
--- Simple installation script for Lua-Gtk.
+-- Simple installation script for LuaGnome.  Run as root.
 -- by Wolfgang Oertl
-
+--
+-- Argument: build directory
 
 require "lfs"
 
@@ -14,19 +15,20 @@ require "lfs"
 -- @param pattern  What to replace in the search path
 -- @param source  File or directory in the current directory to install
 --
-function do_install(ar, pattern, source, dest)
-    local dest2, cmd
+function do_install(ar, pattern, source, basename)
+    local dest, cmd
 
     for path in string.gmatch(ar, "[^;]+") do
 	-- if too short, then it's ./?.so or something.
 	if path:len() > 10 then
-	    dest2 = path:gsub(pattern, dest)
-	    if dest2 ~= path then
-		if lfs.attributes(dest2, "mode") then
+	    dest = path:gsub(pattern, basename)
+	    if dest ~= path then
+		cmd = string.format("cp -a %s %s", source, dest)
+--[[		if lfs.attributes(dest2, "mode") then
 		    print("Destination exists:", dest2)
 		    break
 		end
-		cmd = string.format("cp -a %s %s", source, dest2)
+--]]
 		print(cmd)
 		os.execute(cmd)
 		break
@@ -35,6 +37,15 @@ function do_install(ar, pattern, source, dest)
     end
 end
 
-do_install(package.cpath, "%?.so", "build/linux-i386/gtk.so", "gtk.so")
+build_dir = arg[1]
+assert(build_dir, "Please provide the build directory, or run 'make install'.")
+for dir in lfs.dir(build_dir) do
+    basename = dir .. ".so"
+    file = string.format("%s/%s/%s", build_dir, dir, basename)
+    if lfs.attributes(file, "mode") then
+	do_install(package.cpath, "%?.so", file, basename)
+    end
+end
+
 do_install(package.path, "%?.lua", "lib", "gtk")
 
