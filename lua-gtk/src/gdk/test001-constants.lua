5f10077935eafdcadab74a9b058a26be49b9e826
Wolfgang Oertl <wiz@luaforge.net>
2009-12-23 13:56:56 +0000

    Test to check for correct encoding of constants.

---
+++ lua-gtk/src/gdk/test001-constants.lua
@@ -0,0 +1,11 @@
+#! /usr/bin/env lua
+
+require "gtk"
+
+assert(gdk.GDK_ae == 0x0e6)
+assert(gdk.Return == 0xff0d)
+assert(gdk.Tab == 0xff09)
+assert(gdk.Hyper_R == 0xffee)
+assert(gdk.Ungrab == 0x1008fe20)
+
+
